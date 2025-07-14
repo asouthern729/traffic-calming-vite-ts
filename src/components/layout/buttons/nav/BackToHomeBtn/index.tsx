ab50e1cc932c454082ec83f179799aa0c5c0f2f1
Andrew Southern <andrew.southern@franklintn.gov>
2025-07-14 18:03:59 +0000

    Bug fix on BackToHome button

--- src/components/layout/buttons/nav/BackToHomeBtn/index.tsx
+++ src/components/layout/buttons/nav/BackToHomeBtn/index.tsx
@@ -4,7 +4,7 @@ function BackToHomeBtn() {
 
   return (
     <Link
-      to={'/'}
+      to={'/public'}
       className="text-neutral-content font-[play] font-semibold uppercase text-center py-2 px-3 m-auto w-fit border-2 border-neutral-content rounded hover:text-neutral hover:border-neutral">
         Back To Home
     </Link>
